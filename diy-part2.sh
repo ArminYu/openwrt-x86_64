4e40f3d7bfc701129ed91d04398593c859115fc8
Neko <165999598+ArminYu@users.noreply.github.com>
2024-08-29 20:37:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,6 +17,7 @@ rm -rf feeds/packages/net/smartdns
 rm -rf feeds/packages/net/adguardhome
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-adbyby-plus
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
